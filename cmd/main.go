2a76758d11edf9ded5877ea15ccd6c9454bed68d
Neel <neel@tovala.com>
2023-07-03 15:20:07 -0500

    Adding env reader

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,36 @@ import (
 	"cloud.google.com/go/pubsub"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
+type DatabaseConfig struct {
+	Port     string `env:"PGPORT" env-default:"5432"`
+	Host     string `env:"PGHOST" env-default:"localhost"`
+	Name     string `env:"PGNAME" env-default:"postgres"`
+	User     string `env:"PGUSER" env-default:"user"`
+	Password string `env:"PGPASS" env-default:"password"`
+	SSLMode  bool   `env:"PGSSLENABLED" env-default:"true"`
+}
+
+type AppConfig struct {
+	GCloudProjectID      string `env:"GCLOUD_PROJECTID"`
+	GCloudSubscriptionID string `env:"GCLOUD_SUBSCRIPTIONID"`
+}
+
 func main() {
+	var dbConfig DatabaseConfig
+	err := cleanenv.ReadConfig("config.env", &dbConfig)
+	if err != nil {
+		log.Fatal("Failed to read DatabaseConfig from config.env")
+	}
+
+	var appConfig AppConfig
+	err = cleanenv.ReadConfig("config.env", &appConfig)
+	if err != nil {
+		log.Fatal("Failed to read AppConfig from config.env")
+	}
+
 	r := chi.NewRouter()
 
 	// A good base middleware stack
@@ -29,14 +56,13 @@ func main() {
 		w.Write([]byte("hi"))
 	})
 
-	projectID := "global-walker-125521"
-	client, err := pubsub.NewClient(context.Background(), projectID)
+	client, err := pubsub.NewClient(context.Background(), appConfig.GCloudProjectID)
 	if err != nil {
 		log.Fatal("Failed to create a new secretmanager client")
 	}
 	defer client.Close()
 
-	sub := client.Subscription("nest-monitor-subscription")
+	sub := client.Subscription(appConfig.GCloudSubscriptionID)
 	err = sub.Receive(context.Background(), func(ctx context.Context, m *pubsub.Message) {
 		log.Printf("Got message: %s", m.Data)
 		m.Ack()
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	cloud.google.com/go/pubsub v1.31.0
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/ilyakaznacheev/cleanenv v1.4.2
 )
 
 require (
@@ -12,14 +13,16 @@ require (
 	cloud.google.com/go/compute v1.19.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/iam v1.0.1 // indirect
+	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/s2a-go v0.1.4 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.9.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
 	golang.org/x/sync v0.2.0 // indirect
@@ -30,4 +33,6 @@ require (
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
